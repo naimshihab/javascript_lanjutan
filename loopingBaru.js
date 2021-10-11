2a782475317e6a0bd69d21907abcd014b1b3a88d
Naim Shihab <78292955+naimshihab@users.noreply.github.com>
2021-10-11 12:57:55 +0800

    Add files via upload

---
+++ loopingBaru.js
@@ -0,0 +1,60 @@
+// for of 
+// array
+// const mhs = ['Naim', 'Albion', 'Alim', 'Ciel'];
+
+// mhs.forEach( n => console.log(n) );
+
+// for ( const mahasiswa of mhs ) {
+//   console.log(mahasiswa);
+// }
+
+// --------------------------------------------------------
+
+// String
+// const nama = 'Naim';
+
+// for( const n of nama ) {
+//   console.log(n);
+// }
+
+// --------------------------------------------------------
+
+// const mhs = ['Naim', 'Albion', 'Alim', 'Ciel'];
+
+// mhs.forEach( (m, i) => {
+//   console.log(`${m} adalah mahasiswa ke-${i+1}`);
+// });
+
+// --------------------------------------------------------
+
+// NodeList
+// const liNama = document.querySelectorAll('.nama');
+// liNama.forEach( n => console.log(n.textContent) );
+
+// --------------------------------------------------------
+
+// Arguments
+// function jumlahkanAngka() {
+//   return [1, 2, 3, 4, 5].reduce((a, i) => a + i );
+// }
+
+// console.log(jumlahkanAngka(1, 2, 3, 4, 5));
+
+// --------------------------------------------------------
+
+// For in
+// const mhs = {
+//   nama: 'Naim Shihab',
+//   umur: 18,
+//   kelas: 'software engineering'
+// }
+
+// for ( const m in mhs ) {
+//   console.log(mhs[m]);
+// }
+
+
+
+
+
+
